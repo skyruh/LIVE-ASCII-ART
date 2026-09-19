b8ba0cc33636a63bd7d2fc35c9b553cbc01be22e
Shivansh Gupta <shivansh.random@gmail.com>
2026-09-19 18:53:59 +0530

    GLFW: window made

---
+++ src/app/window.cpp
@@ -0,0 +1,42 @@
+#include <GLFW/glfw3.h>
+#include "../common/config.cpp"
+
+int main()
+{
+    AppConfig config;
+    GLFWwindow* window;
+
+    int height = config.window_height;
+    int width = config.window_width;
+    const char* title = config.window_title;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
